1f165f193e258015176c4939ad02a37d70eeb930
Surendhiran <sureymadhu@gmail.com>
2024-01-30 17:23:24 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,1 +1,10 @@
- docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+docker volume create --name sonarqube_data
+docker volume create --name sonarqube_logs
+docker volume create --name sonarqube_extensions
+
+docker run -d --name sonarqube \
+    -p 9000:9000 \
+    -v sonarqube_data:/opt/sonarqube/data \
+    -v sonarqube_extensions:/opt/sonarqube/extensions \
+    -v sonarqube_logs:/opt/sonarqube/logs \
+    sonarqube:lts-community
